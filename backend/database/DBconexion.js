a27d11aab9e3fc455a3edab23dddac8f3e79fc28
josue <josueandres.ap@gmail.com>
2024-11-17 18:56:20 -0300

    Integracion de Dotenv

--- backend/database/DBconexion.js
+++ backend/database/DBconexion.js
@@ -1,18 +1,19 @@
-import mysql from 'mysql';
+import mysql from "mysql";
+import "dotenv/config";
 
 const db = mysql.createConnection({
-  host: 'localhost', 
-  user: 'root', 
-  password: '12345678',
-  database: 'orquigestion'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
-db.connect(err => {
+db.connect((err) => {
   if (err) {
-    console.error('Error de conexion a la base de datos:', err);
+    console.error("Error de conexion a la base de datos:", err);
     return;
   }
-  console.log('conexion exitosa a la base de datos');
+  console.log("conexion exitosa a la base de datos");
 });
 
 export default db;
